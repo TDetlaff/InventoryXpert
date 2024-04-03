1f3fa049d47d52b62de7917bd9b5a3b56f2687e9
Taylor Detlaff <tdetlaff@student.cvtc.edu>
2024-04-03 18:17:03 -0500

    Add project structure

---
+++ InventoryXpert/InventoryXpert/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventoryXpert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
